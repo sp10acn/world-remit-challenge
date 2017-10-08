aa5d27a05df11aca14a7f13dc9356614a6a9a529
Sam Phillips <sdp.172@gmail.com>
2017-10-08 11:46:49 +0100

    Added better test coverage for MainComponent.

--- so-users-react/src/components/main/MainComponent.js
+++ so-users-react/src/components/main/MainComponent.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CircularProgress from 'material-ui/CircularProgress';
 import List from 'material-ui/List';
-import ListItem from 'material-ui/List';
+import ListItem from 'material-ui/List/ListItem';
 import Avatar from 'material-ui/Avatar';
 import Subheader from 'material-ui/Subheader';
 
 class MainComponent extends Component{
     render(){
-        const error = this.props.error ? true : false;
+        const error = this.props.error || false;
         const errorMessage = this.props.errorMessage || "";
         const loading = this.props.loading || false;
-        const users = this.props.users || [];
+        const users = this.props.users || null;
 
         const errorStyles = {
             textAlign: "center",
--- so-users-react/src/components/main/MainComponent.test.js
+++ so-users-react/src/components/main/MainComponent.test.js
@@ -4,10 +4,28 @@ import { shallow } from 'enzyme';
 import expect from 'expect';
 
 describe("MainComponent", () => {
+    it("should display error", () => {
+        let wrapper = shallow(<MainComponent />);
+        expect(wrapper.find("CircularProgress").length).toEqual(0);
+        expect(wrapper.find("List").length).toEqual(0);
+        expect(wrapper.find("ListItem").length).toEqual(0);
+        expect(wrapper.find("h3").length).toEqual(1);
+    });
+
+    it("should display error", () => {
+        let wrapper = shallow(<MainComponent error={true} errorMessage={"ERROR"} loading={true} users={[]} />);
+        expect(wrapper.find("CircularProgress").length).toEqual(0);
+        expect(wrapper.find("List").length).toEqual(0);
+        expect(wrapper.find("ListItem").length).toEqual(0);
+        expect(wrapper.find("h3").length).toEqual(1);
+    });
+
     it("should display loading", () => {
         let wrapper = shallow(<MainComponent loading={true} users={undefined}/>);
         expect(wrapper.find("CircularProgress").length).toEqual(1);
         expect(wrapper.find("List").length).toEqual(0);
+        expect(wrapper.find("ListItem").length).toEqual(0);
+        expect(wrapper.find("h3").length).toEqual(0);
     });
 
     it("should display users", () => {
@@ -26,8 +44,9 @@ describe("MainComponent", () => {
             }
         ];
         let wrapper = shallow(<MainComponent loading={false} users={users}/>);
-        expect(wrapper.find("Loading").length).toEqual(0);
+        expect(wrapper.find("CircularProgress").length).toEqual(0);
         expect(wrapper.find("List").length).toEqual(1);
         expect(wrapper.find("ListItem").length).toEqual(3);
+        expect(wrapper.find("h3").length).toEqual(0);
     });
 });
